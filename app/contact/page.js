a16e6afec8731a6e3d8f1862f4a0cbb5f99a8559
your_username <rahulpaul199914@gmail.com>
2025-04-14 13:27:15 +0600

    completed contanct page

--- app/contact/page.js
+++ app/contact/page.js
@@ -4,12 +4,12 @@ export default function Contact() {
     
   return (
     <>
-      <div className="container heroElem w-full m-auto flex justify-center text-center flex-col items-center">
-          <div className="sub">
+      <div className="container contact heroElem w-full h-dvh m-auto flex lg:justify-start md:justify-center justify-start text-center flex-col items-center">
+          <div className="sub ">
               Got a question, proposal or project or want to work <br />
               together on something? Feel free to reach out.
             </div>
-            <form className="w-full md:w-[70%]">
+            <form className="w-full md:w-[70%] mb-8">
               <div className="md:flex justify-between">
                 <div className="side">
                     <label htmlFor="name">Your Name</label>
@@ -40,8 +40,27 @@ export default function Contact() {
                   required=""
                 ></textarea>
               </div>
-              <button type="submit">Send Message.</button>
+              <button className="md:mb-0 mb-12" type="submit">Send Message.</button>
             </form>
+            <div className="mb-4 hidden lg:flex items-center justify-center">
+                <span className="w-10 bg-gradient-to-r to-[#99bbcc] from-[#1d5f80] h-[1px]"></span>
+                <span className="text-xs text-center text-gray-500 px-3">Or</span>
+                <span className="w-10 bg-gradient-to-l to-[#99bbcc] from-[#1d5f80] h-[1px]"></span>
+            </div>
+                        <div className="lg:static fixed left-0 top-1/2 transform -translate-y-1/2 flex justify-center lg:flex-row flex-col gap-2.5 items-center mb-6 lg:bg-transparent bg-blue-900 rounded-r-2xl">
+                            <a href="mailto:rahulpaul9914@gmail.com" className="bg-light px-3 py-2 flex items-center gap-2 rounded-lg w-fit">
+                                <img className="w-6 h-6" src="/images/social/google.png" alt="google"/>
+                                <span className="lg:block hidden">Google</span>
+                            </a>
+                            <a href="https://www.facebook.com/profile.php?id=100009216620059" className="bg-light px-3 py-2 flex items-center gap-2 rounded-lg w-fit">
+                                <img className="w-6 h-6" src="/images/social/Facebook.png" alt="facebook"/>
+                                <span className="lg:block hidden">Facebook</span>
+                            </a>
+                            <a href="https://bd.linkedin.com/in/rahul-paul-768165184" className="bg-light px-3 py-2 flex items-center gap-2 rounded-lg w-fit">
+                                <img className="w-6 h-6" src="/images/social/linkedin.png" alt="linkedin"/>
+                                <span className="lg:block hidden">Twitter</span>
+                            </a>
+                        </div>
       </div>
     </>
   );
--- layouts/MobileLayout.js
+++ layouts/MobileLayout.js
@@ -4,8 +4,8 @@ import routes from '@/public/data/routs.json'
 export default function MobileLayout (){
     
     return (
-        <nav className="lg:hidden block fixed bottom-8 sm:top-8 left-1/2 transform -translate-x-1/2 z-[999]">
-          <ul className="flex items-center gap-x-10 bg-white/10 max-w-xs w-full p-4 rounded-full">
+        <nav className="lg:hidden block fixed bottom-0 sm:bottom-8 sm:top-8 left-1/2 transform -translate-x-1/2 z-[999] sm:w-fit w-full">
+          <ul className="flex items-center justify-between gap-x-10 bg-[#021234] sm:max-w-xs w-full p-4 rounded-full">
             {routes.map((item, index) => (
               <li key={index} className="list-none text-white opacity-40 hover:opacity-100 transition-opacity">
                 <Link href={item.path} className="p-3">
